4bfece022f8c8282593d0afdbc17ca96bc59d167
CamiloQuijano <camiloquijano31@hotmail.com>
2013-10-28 23:06:02 -0500

    menu, setup con modulos, pendiente validacion de acceso en controladores

--- source/DB/setup.sql
+++ source/DB/setup.sql
@@ -1,5 +1,13 @@
-INSERT INTO `tbl_modulo`
-VALUES (1, 'home', 1, 'homepage');
+INSERT INTO `tbl_modulo` VALUES 
+(1, 'home', 1, 'homepage'),
+(2, 'cargos', 1, 'cargo,cargo_show,cargo_new,cargo_edit,cargo_delete'),
+(3, 'departamentos', 1, 'departamento,departamento_show,departamento_new,departamento_edit,departamento_delete'),
+(4, 'organizaciones', 1, 'organizacion,organizacion_show,organizacion_new,organizacion_edit,organizacion_delete'),
+(5, 'estados_proyecto', 1, 'estadoproyecto,estadoproyecto_show,estadoproyecto_new,estadoproyecto_edit,estadoproyecto_delete'),
+(6, 'linea_investigacion', 1, 'lineainvestigacion,lineainvestigacion_show,lineainvestigacion_new,lineainvestigacion_edit,lineainvestigacion_delete'),
+(7, 'tipo_investigacion', 1, 'tipoinvestigacion,tipoinvestigacion_show,tipoinvestigacion_new,tipoinvestigacion_edit,tipoinvestigacion_delete'),
+(8, 'errores', 1, 'errores'),
+(9, 'auditoria', 1, 'auditoria');
 
 INSERT INTO `tbl_perfil` (`id`, `per_perfil`, `per_estado`) VALUES (1, 'Superadministrador', '1');
 INSERT INTO `tbl_perfil` (`id`, `per_perfil`, `per_estado`) VALUES (2, 'Administrador', '1');
@@ -10,10 +18,21 @@ INSERT INTO `tbl_perfil_modulo` (`perfil_id`, `modulo_id`) VALUES
 ('1', '1'),
 ('2', '1'),
 ('3', '1'),
-('4', '1');
-
-
-
+('4', '2'),
+('1', '2'),
+('2', '2'),
+('1', '3'),
+('2', '3'),
+('1', '4'),
+('2', '4'),
+('1', '5'),
+('2', '5'),
+('1', '6'),
+('2', '6'),
+('1', '7'),
+('2', '7'),
+('1', '8'),
+('1', '9');
 
 INSERT INTO `tbl_usuario`
 (`id`,
@@ -51,7 +70,7 @@ INSERT INTO `tbl_usuario`
 `departamento_id`,
 `organizacion_id`)
 VALUES
-(null,
+(2,
 '10123456',
 'Camilo Quijano',
 '2013-10-15',
@@ -62,8 +81,33 @@ null,
 null,
 null);
 
+INSERT INTO `tbl_usuario`
+(`id`,
+`usu_cedula`,
+`usu_nombre`,
+`usu_fecha_creacion`,
+`usu_log`,
+`usu_password`,
+`usu_estado`,
+`cargo_id`,
+`departamento_id`,
+`organizacion_id`)
+VALUES
+(3,
+'6666666666',
+'Juan Perez',
+'2013-10-15',
+'juanperez@hotmail.com',
+'14a8dc6c9e4b8f6e3228576bec334cb78179668e',
+1,
+null,
+null,
+null);
+
 INSERT INTO `tbl_usuario_perfil` (`usuario_id`, `perfil_id`) VALUES ('1', '1');
 INSERT INTO `tbl_usuario_perfil` (`usuario_id`, `perfil_id`) VALUES ('2', '1');
+INSERT INTO `tbl_usuario_perfil` (`usuario_id`, `perfil_id`) VALUES ('3', '3');
+
 
 
 
