9546cfad1ae07bf484fc777a2b31af96e4aa0eba
diego-software@hotmail.com <diego-software@hotmail.com>
2013-11-10 10:28:07 -0500

    correccion de error de importacino

--- source/src/sgii/sgiiBundle/Services/ImportService.php
+++ source/src/sgii/sgiiBundle/Services/ImportService.php
@@ -46,18 +46,21 @@ class ImportService
         
         
         $data = array();
-//        for($i = 1; $i <= 50; $i++)
-//        {
-//            $data[$i]['usuCedula'] = $objExcel->getActiveSheet()->getCell('A'.$i)->getCalculatedValue();
-//            $data[$i]['usuNombre'] = $objExcel->getActiveSheet()->getCell('B'.$i)->getCalculatedValue();
-//            $data[$i]['usuApellido'] = $objExcel->getActiveSheet()->getCell('C'.$i)->getCalculatedValue();
-//            $data[$i]['usuLog'] = $objExcel->getActiveSheet()->getCell('D'.$i)->getCalculatedValue();
-//            $data[$i]['cargo'] = $objExcel->getActiveSheet()->getCell('E'.$i)->getCalculatedValue();
-//            $data[$i]['nivel'] = $objExcel->getActiveSheet()->getCell('F'.$i)->getCalculatedValue();
-//            $data[$i]['departamento'] = $objExcel->getActiveSheet()->getCell('G'.$i)->getCalculatedValue();
-//            $data[$i]['organizacion'] = $objExcel->getActiveSheet()->getCell('H'.$i)->getCalculatedValue();
-//        }
+        for($i = 1; $i <= 50; $i++)
+        {
+            $data[$i]['usuCedula'] = $objExcel->getActiveSheet()->getCell('A'.$i)->getCalculatedValue();
+            $data[$i]['usuNombre'] = $objExcel->getActiveSheet()->getCell('B'.$i)->getCalculatedValue();
+            $data[$i]['usuApellido'] = $objExcel->getActiveSheet()->getCell('C'.$i)->getCalculatedValue();
+            $data[$i]['usuLog'] = $objExcel->getActiveSheet()->getCell('D'.$i)->getCalculatedValue();
+            $data[$i]['cargo'] = $objExcel->getActiveSheet()->getCell('E'.$i)->getCalculatedValue();
+            $data[$i]['nivel'] = $objExcel->getActiveSheet()->getCell('F'.$i)->getCalculatedValue();
+            $data[$i]['departamento'] = $objExcel->getActiveSheet()->getCell('G'.$i)->getCalculatedValue();
+            $data[$i]['organizacion'] = $objExcel->getActiveSheet()->getCell('H'.$i)->getCalculatedValue();
+        }
         
+		// Eliminar archivo temporal
+		unlink($path);
+		
         echo "<pre>";
         print_r($data);
         echo "</pre>";
